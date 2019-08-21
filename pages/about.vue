27892238a1af8bdd3798525cc37818e18b12271b
= <radlohead@naver.com>
2019-08-22 08:20:16 +0900

    about head 속성 추가 및 변경

--- pages/about.vue
+++ pages/about.vue
@@ -7,6 +7,14 @@
 export default {
   asyncData() {
     console.log(process.server); // false이면 csr이라는 의미
+  },
+  head() {
+    return {
+      title: "커스텀 타이틀",
+      meta: [
+        { hid: "유니크한 아이디", name: "설명", content: "내가 커스텀한 설명" }
+      ]
+    };
   }
 };
 </script>
